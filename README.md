ad7156734409b4d13c011130738971f3ce11edb2
LeoDeg <leodeg.dev@gmail.com>
2020-12-24 13:04:05 +0300

    feat: complete tutorial

---
+++ README.md
@@ -0,0 +1,36 @@
+# Keras: How to get started with Keras
+
+Tutorial: [Keras Tutorial: How to get started with Keras, Deep Learning, and Python](https://www.pyimagesearch.com/2018/09/10/keras-tutorial-how-to-get-started-with-keras-deep-learning-and-python/)
+
+## Dependencies
+
+1. matplotlib
+2. numpy
+3. tensorflow
+4. keras
+5. scikit-learn
+6. opencv
+
+## Results
+
+![Dog](results/cat.jpg)
+
+![Dog](results/cat2.jpg)
+
+![Dog](results/cat3.jpg)
+
+![Dog](results/dog.jpg)
+
+![Dog](results/dog2.jpg)
+
+![Dog](results/dog3.jpg)
+
+![Dog](results/dog4.jpg)
+
+![Dog](results/panda.jpg)
+
+![Dog](results/panda2.jpg)
+
+![Dog](results/panda3.jpg)
+
+![Dog](results/panda4.jpg)
---
+++ predict.py
@@ -0,0 +1,81 @@
+# USAGE
+# python predict.py --image images/dog.jpg --model output/simple_nn.model --label-bin output/simple_nn_lb.pickle --width 32 --height 32 --flatten 1
+# python predict.py --image images/dog.jpg --model output/smallvggnet.model --label-bin output/smallvggnet_lb.pickle --width 64 --height 64
+
+# import the necessary packages
+from tensorflow.keras.models import load_model
+import argparse
+import pickle
+import cv2
+import ntpath
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True,
+	help="path to input image we are going to classify")
+ap.add_argument("-m", "--model", required=True,
+	help="path to trained Keras model")
+ap.add_argument("-l", "--label-bin", required=True,
+	help="path to label binarizer")
+
+ap.add_argument("-w", "--width", type=int, default=28,
+	help="target spatial dimension width")
+ap.add_argument("-e", "--height", type=int, default=28,
+	help="target spatial dimension height")
+ap.add_argument("-f", "--flatten", type=int, default=-1,
+	help="whether or not we should flatten the image")
+
+ap.add_argument("-sh", "--show", type=bool, default=True,
+	help="should or not show image on window")
+ap.add_argument("-s", "--save", type=bool, default=True,
+	help="should or not save image result to results folder")
+
+args = vars(ap.parse_args())
+
+# load the input image and resize it to the target spatial dimensions
+image = cv2.imread(args["image"])
+output = image.copy()
+image = cv2.resize(image, (args["width"], args["height"]))
+
+# scale the pixel values to [0, 1]
+image = image.astype("float") / 255.0
+
+# check to see if we should flatten the image and add a batch
+# dimension
+if args["flatten"] > 0:
+	image = image.flatten()
+	image = image.reshape((1, image.shape[0]))
+
+# otherwise, we must be working with a CNN -- don't flatten the
+# image, simply add the batch dimension
+else:
+	image = image.reshape((1, image.shape[0], image.shape[1],
+		image.shape[2]))
+
+# load the model and label binarizer
+print("[INFO] loading network and label binarizer...")
+model = load_model(args["model"])
+lb = pickle.loads(open(args["label_bin"], "rb").read())
+
+# make a prediction on the image
+preds = model.predict(image)
+
+# find the class label index with the largest corresponding
+# probability
+i = preds.argmax(axis=1)[0]
+label = lb.classes_[i]
+
+# draw the class label + probability on the output image
+text = "{}: {:.2f}%".format(label, preds[0][i] * 100)
+cv2.putText(output, text, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1,
+	(0, 0, 255), 2)
+
+if args["save"]:
+	# save image to folder
+	head, tail = ntpath.split(args["image"])
+	cv2.imwrite('results/' + tail, output)
+
+if args["show"]:
+	# show the output image
+	cv2.imshow("Image", output)
+	cv2.waitKey(0)
